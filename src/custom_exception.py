6952636612faea8f408ffa0d2653d0d3906f3366
princebeersingh <princebeersingh@gmail.com>
2025-07-26 05:49:40 +0000

    some exception error

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -2,20 +2,20 @@ import traceback
 import sys
 
 class CustomException(Exception):
-      def __init__(self, error_message, error_detail: sys):
-            super().__init__(error_message)
-            self.error_message = self.get_detailed_error_message(error_message, error_detail)
 
-      @staticmethod
-      def get_detailed_error_message(error_message, error_detail: sys):
-            exc_type, exc_obj, exc_tb = traceback.sys.exc_info()
-            if exc_tb is not None:
-                  file_name = exc_tb.tb_frame.f_code.co_filename
-                  line_number = exc_tb.tb_lineno
-            else:
-                  file_name = "unknown"
-                  line_number = "unknown"
-            return f"Error in {file_name} at line {line_number}: {error_message}"
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = self.get_detailed_error_message(error_message,error_detail)
 
-      def __str__(self):
-            return self.error_message
+    @staticmethod
+    def get_detailed_error_message(error_message , error_detail:sys):
+
+        _, _, exc_tb = traceback.sys.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+
+        return f"Error in {file_name} , line {line_number} : {error_message}"
+    
+    def __str__(self):
+        return self.error_message
+
